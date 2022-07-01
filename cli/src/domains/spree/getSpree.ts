4b04496cbd93f248bd6da8be1612c8862deb1f7a
letelete <brunokawka@gmail.com>
2022-07-01 15:37:00 +0200

    Fix eslint issues

--- cli/src/domains/spree/getSpree.ts
+++ cli/src/domains/spree/getSpree.ts
@@ -1,5 +1,5 @@
-import inquirer from "inquirer";
-import type Spree from "./Spree";
+import inquirer from 'inquirer';
+import type Spree from './Spree';
 
 type Answers = {
   spree: Spree;
@@ -11,17 +11,17 @@ type Options = {
 
 const spreeTemplates = [
   {
-    name: "Spree (dockerized)",
-    version: "docker",
-    gitRepositoryURL: "https://github.com/spree/spree_starter",
-    documentationURL: "https://dev-docs.spreecommerce.org/",
+    name: 'Spree (dockerized)',
+    version: 'docker',
+    gitRepositoryURL: 'https://github.com/spree/spree_starter',
+    documentationURL: 'https://dev-docs.spreecommerce.org/'
   },
   {
-    name: "Spree (no docker)",
-    version: "no-docker",
-    gitRepositoryURL: "https://github.com/spree/spree_starter",
-    documentationURL: "https://dev-docs.spreecommerce.org/",
-  },
+    name: 'Spree (no docker)',
+    version: 'no-docker',
+    gitRepositoryURL: 'https://github.com/spree/spree_starter',
+    documentationURL: 'https://dev-docs.spreecommerce.org/'
+  }
 ];
 
 /** Gets the integration from user's input. */
@@ -30,19 +30,19 @@ const getSpree = async (options: Options): Promise<Spree> => {
 
   const choices = [...spreeTemplates].map((spree) => ({
     name: spree.name,
-    value: spree,
+    value: spree
   }));
 
   const answers = await inquirer.prompt<Answers>({
     choices,
     message,
-    type: "list",
-    name: "spree",
+    type: 'list',
+    name: 'spree'
   });
   if (answers.spree.gitRepositoryURL) return answers.spree;
 
   return {
-    ...answers.spree,
+    ...answers.spree
   };
 };
 
