2e61736d162c19df02907b05d9e1e45bced4e437
Rafal Kosla <rafalkosla101@gmail.com>
2022-07-01 09:56:21 +0200

    Add funcionality to choose spree variant

---
+++ cli/src/domains/spree/getSpree.ts
@@ -0,0 +1,55 @@
+import inquirer from 'inquirer';;
+import type Integration from "../integration/Integration";
+
+type Answers = {
+  integration: Integration;
+};
+
+/** The answers expected in the form of 'inquirer'. */
+
+type Options = {
+  message: string;
+};
+
+const integrations = [
+  {
+    "id": "808ce800-24b0-4228-aa61-966f399b3ea3",
+    "name": "Spree (dockerized)",
+    "gitRepositoryURL": "https://github.com/spree/spree_starter",
+    "createdAt": "2022-02-23T04:55:48.881Z",
+    "updatedAt": "2022-02-24T00:10:31.612Z",
+    "documentationURL": "https://dev-docs.spreecommerce.org/"
+  },
+  {
+    "id": "0a2083c5-4835-48fc-9143-212572fc66b9",
+    "name": "Spree (no docker)",
+    "gitRepositoryURL": "https://github.com/spree/spree_starter",
+    "createdAt": "2022-02-23T04:57:08.922Z",
+    "updatedAt": "2022-02-24T00:11:53.291Z",
+    "documentationURL": "https://dev-docs.spreecommerce.org/"
+  }
+]
+
+/** Gets the integration from user's input. */
+const getSpree = async (options: Options): Promise<Integration> => {
+  const {message} = options;
+
+  const choices = [...integrations].map((integration) => ({
+    name: integration.name,
+    value: integration
+  }));
+
+  const answers = await inquirer.prompt<Answers>({
+    choices,
+    message,
+    type: 'list',
+    name: 'integration'
+  });
+  if (answers.integration.gitRepositoryURL) return answers.integration;
+
+  return {
+    ...answers.integration
+  };
+};
+
+export default getSpree;
---
+++ cli/src/domains/spree/index.ts
@@ -0,0 +1,1 @@
+export { default as getSpree } from './getSpree';
