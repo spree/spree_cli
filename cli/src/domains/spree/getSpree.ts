65a824d3e757497b55418f73737a916004167d33
Rafal Kosla <rafalkosla101@gmail.com>
2022-07-01 11:17:21 +0200

    Fix types and add structure

--- cli/src/domains/spree/getSpree.ts
+++ cli/src/domains/spree/getSpree.ts
@@ -1,56 +1,48 @@
-import inquirer from 'inquirer';;
-import type Integration from "../integration/Integration";
+import inquirer from "inquirer";
+import type Spree from "./Spree";
 
 type Answers = {
-  integration: Integration;
+  spree: Spree;
 };
-
-/** The answers expected in the form of 'inquirer'. */
 
 type Options = {
   message: string;
 };
 
-const integrations = [
+const spreeTemplates = [
   {
-    "id": "808ce800-24b0-4228-aa61-966f399b3ea3",
-    "name": "Spree (dockerized)",
-    "version": "docker",
-    "gitRepositoryURL": "https://github.com/spree/spree_starter",
-    "createdAt": "2022-02-23T04:55:48.881Z",
-    "updatedAt": "2022-02-24T00:10:31.612Z",
-    "documentationURL": "https://dev-docs.spreecommerce.org/"
+    name: "Spree (dockerized)",
+    version: "docker",
+    gitRepositoryURL: "https://github.com/spree/spree_starter",
+    documentationURL: "https://dev-docs.spreecommerce.org/",
   },
   {
-    "id": "0a2083c5-4835-48fc-9143-212572fc66b9",
-    "name": "Spree (no docker)",
-    "version": "no-docker",
-    "gitRepositoryURL": "https://github.com/spree/spree_starter",
-    "createdAt": "2022-02-23T04:57:08.922Z",
-    "updatedAt": "2022-02-24T00:11:53.291Z",
-    "documentationURL": "https://dev-docs.spreecommerce.org/"
-  }
-]
+    name: "Spree (no docker)",
+    version: "no-docker",
+    gitRepositoryURL: "https://github.com/spree/spree_starter",
+    documentationURL: "https://dev-docs.spreecommerce.org/",
+  },
+];
 
 /** Gets the integration from user's input. */
-const getSpree = async (options: Options): Promise<Integration> => {
-  const {message} = options;
+const getSpree = async (options: Options): Promise<Spree> => {
+  const { message } = options;
 
-  const choices = [...integrations].map((integration) => ({
-    name: integration.name,
-    value: integration
+  const choices = [...spreeTemplates].map((spree) => ({
+    name: spree.name,
+    value: spree,
   }));
 
   const answers = await inquirer.prompt<Answers>({
     choices,
     message,
-    type: 'list',
-    name: 'integration'
+    type: "list",
+    name: "spree",
   });
-  if (answers.integration.gitRepositoryURL) return answers.integration;
+  if (answers.spree.gitRepositoryURL) return answers.spree;
 
   return {
-    ...answers.integration
+    ...answers.spree,
   };
 };
 
