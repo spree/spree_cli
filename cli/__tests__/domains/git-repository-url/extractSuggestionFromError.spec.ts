upsidelab/spree_starter

Initial commit with oclif cli

cli/__tests__/domains/git-repository-url/extractSuggestionFromError.spec.ts
@@ -1,0 +1,56 @@
+import git from 'isomorphic-git';
+import { extractSuggestionFromError, validateGitRepositoryURL } from '../../../src/domains/git-repository-url';
+
+jest.mock('../../../src/domains/git-repository-url/validateGitRepositoryURL');
+
+type ValidateGitRepositoryURL = jest.MockedFunction<typeof validateGitRepositoryURL>;
+
+describe('extractSuggestionFromError | unit tests', () => {
+  it('extracts suggested git repository URL', async () => {
+    const error = new git.Errors.UnknownTransportError(
+      'git@github.com:vuestorefront/test-store.git',
+      'ssh',
+      'https://github.com/vuestorefront/test-store.git'
+    );
+
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([true, null]);
+
+    expect(await extractSuggestionFromError(error)).toBe('https://github.com/vuestorefront/test-store.git');
+  });
+
+  it('returns \'null\' when there\'s no suggestion', async () => {
+    const errorA = new git.Errors.UnknownTransportError('git@github.com:vuestorefront/test-store.git', 'ssh');
+
+    expect(await extractSuggestionFromError(errorA)).toBeNull();
+
+    const errorB = new git.Errors.UrlParseError('https://github.com/companyvue-storefront-template.git');
+
+    expect(await extractSuggestionFromError(errorB)).toBeNull();
+
+    const errorC = new Error('The button exploded!');
+
+    expect(await extractSuggestionFromError(errorC)).toBeNull();
+  });
+
+  it('checks if suggestion is valid before returning it', async () => {
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([false, new Error('An expected error')]);
+
+    const errorA = new git.Errors.UnknownTransportError(
+      'git@github.com:vuestorefront/test-store.git',
+      'ssh',
+      'https://github.com/vuestorefront/test-store.git'
+    );
+
+    expect(await extractSuggestionFromError(errorA)).toBeNull();
+
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([true, null]);
+
+    const errorB = new git.Errors.UnknownTransportError(
+      'git@github.com:vuestorefront/test-store.git',
+      'ssh',
+      'https://github.com/vuestorefront/test-store.git'
+    );
+
+    expect(await extractSuggestionFromError(errorB)).toBe('https://github.com/vuestorefront/test-store.git');
+  });
+});

cli/__tests__/domains/git-repository-url/validateGitRepositoryURL.spec.ts
@@ -1,0 +1,45 @@
+import { validateGitRepositoryURL } from '../../../src/domains/git-repository-url';
+import git from 'isomorphic-git';
+
+describe('validateGitRepositoryURL | unit tests', () => {
+  const url = 'https://github.com/vuestorefront/test-template.git';
+
+  describe('when receives a valid git repository URL', () => {
+    beforeEach(() => {
+      jest.spyOn(git, 'getRemoteInfo2').mockResolvedValueOnce({
+        protocolVersion: 2,
+        capabilities: {
+          agent: 'git/github-gf33fef312227',
+          fetch: 'shallow wait-for-done filter',
+          'ls-refs': 'unborn',
+          'server-option': true,
+          'object-format': 'sha1'
+        }
+      });
+    });
+
+    it('uses git remote info to check it', async () => {
+      await validateGitRepositoryURL(url);
+
+      expect(git.getRemoteInfo2).toHaveBeenCalledWith(
+        expect.objectContaining({
+          url
+        })
+      );
+    });
+
+    it('returns \'true\' and no error', async () => {
+      expect(await validateGitRepositoryURL(url)).toEqual([true, null]);
+    });
+  });
+
+  describe('when receives an invalid git repository URL', () => {
+    it('returns \'false\' and an error', async () => {
+      const error = new git.Errors.UrlParseError(url);
+
+      jest.spyOn(git, 'getRemoteInfo2').mockRejectedValueOnce(error);
+
+      expect(await validateGitRepositoryURL(url)).toEqual([false, error]);
+    });
+  });
+});

cli/__tests__/domains/git-repository/cloneGitRepository.spec.ts
@@ -1,0 +1,66 @@
+import git from 'isomorphic-git';
+import { wait } from '../../../src/domains/async';
+import { cloneGitRepository } from '../../../src/domains/git-repository';
+
+jest.mock('isomorphic-git');
+
+describe('cloneGitRepository | unit tests', () => {
+  let output = '';
+
+  beforeEach(() => {
+    output = '';
+
+    jest.spyOn(process.stderr, 'write').mockImplementation((message) => {
+      output += message as string;
+      return true;
+    });
+  });
+
+  it('clones git repository to the project directory', async () => {
+    const projectDir = '~/Projects/test-store';
+
+    const gitRepositoryURL = 'https://github.com/vuestorefront/test-template.git';
+
+    jest.spyOn(git, 'clone').mockResolvedValueOnce();
+
+    await cloneGitRepository({
+      projectDir,
+      gitRepositoryURL
+    });
+
+    expect(git.clone).toHaveBeenCalledWith(
+      expect.objectContaining({
+        dir: projectDir,
+        url: gitRepositoryURL
+      })
+    );
+  });
+
+  it('displays progress bar that reflects cloned files', async () => {
+    jest.spyOn(git, 'clone').mockImplementationOnce(async (options) => {
+      await wait(100);
+
+      options.onProgress?.({
+        phase: 'Counting objects',
+        total: 2,
+        loaded: 1
+      });
+
+      await wait(100);
+
+      options.onProgress?.({
+        phase: 'Counting objects',
+        total: 2,
+        loaded: 2
+      });
+    });
+
+    await cloneGitRepository({
+      projectDir: '~/Projects/test-store',
+      gitRepositoryURL: 'https://github.com/vuestorefront/test-template.git'
+    });
+
+    expect(output).toContain('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ || 0%');
+    expect(output).toContain('████████████████████████████████████████ || 100%');
+  });
+});

spree/spree_cli

cli/__tests__/domains/git-repository-url/extractSuggestionFromError.spec.ts
@@ -1,0 +1,56 @@
+import git from 'isomorphic-git';
+import { extractSuggestionFromError, validateGitRepositoryURL } from '../../../src/domains/git-repository-url';
+
+jest.mock('../../../src/domains/git-repository-url/validateGitRepositoryURL');
+
+type ValidateGitRepositoryURL = jest.MockedFunction<typeof validateGitRepositoryURL>;
+
+describe('extractSuggestionFromError | unit tests', () => {
+  it('extracts suggested git repository URL', async () => {
+    const error = new git.Errors.UnknownTransportError(
+      'git@github.com:upsidelab/test-store.git',
+      'ssh',
+      'https://github.com/upsidelab/test-store.git'
+    );
+
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([true, null]);
+
+    expect(await extractSuggestionFromError(error)).toBe('https://github.com/upsidelab/test-store.git');
+  });
+
+  it('returns \'null\' when there\'s no suggestion', async () => {
+    const errorA = new git.Errors.UnknownTransportError('git@github.com:upsidelab/test-store.git', 'ssh');
+
+    expect(await extractSuggestionFromError(errorA)).toBeNull();
+
+    const errorB = new git.Errors.UrlParseError('https://github.com/upsidelab-template.git');
+
+    expect(await extractSuggestionFromError(errorB)).toBeNull();
+
+    const errorC = new Error('The button exploded!');
+
+    expect(await extractSuggestionFromError(errorC)).toBeNull();
+  });
+
+  it('checks if suggestion is valid before returning it', async () => {
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([false, new Error('An expected error')]);
+
+    const errorA = new git.Errors.UnknownTransportError(
+      'git@github.com:upsidelab/test-store.git',
+      'ssh',
+      'https://github.com/upsidelab/test-store.git'
+    );
+
+    expect(await extractSuggestionFromError(errorA)).toBeNull();
+
+    (validateGitRepositoryURL as ValidateGitRepositoryURL).mockResolvedValueOnce([true, null]);
+
+    const errorB = new git.Errors.UnknownTransportError(
+      'git@github.com:upsidelab/test-store.git',
+      'ssh',
+      'https://github.com/upsidelab/test-store.git'
+    );
+
+    expect(await extractSuggestionFromError(errorB)).toBe('https://github.com/upsidelab/test-store.git');
+  });
+});

cli/__tests__/domains/git-repository-url/validateGitRepositoryURL.spec.ts
@@ -1,0 +1,45 @@
+import { validateGitRepositoryURL } from '../../../src/domains/git-repository-url';
+import git from 'isomorphic-git';
+
+describe('validateGitRepositoryURL | unit tests', () => {
+  const url = 'https://github.com/upsidelab/test-template.git';
+
+  describe('when receives a valid git repository URL', () => {
+    beforeEach(() => {
+      jest.spyOn(git, 'getRemoteInfo2').mockResolvedValueOnce({
+        protocolVersion: 2,
+        capabilities: {
+          agent: 'git/github-gf33fef312227',
+          fetch: 'shallow wait-for-done filter',
+          'ls-refs': 'unborn',
+          'server-option': true,
+          'object-format': 'sha1'
+        }
+      });
+    });
+
+    it('uses git remote info to check it', async () => {
+      await validateGitRepositoryURL(url);
+
+      expect(git.getRemoteInfo2).toHaveBeenCalledWith(
+        expect.objectContaining({
+          url
+        })
+      );
+    });
+
+    it('returns \'true\' and no error', async () => {
+      expect(await validateGitRepositoryURL(url)).toEqual([true, null]);
+    });
+  });
+
+  describe('when receives an invalid git repository URL', () => {
+    it('returns \'false\' and an error', async () => {
+      const error = new git.Errors.UrlParseError(url);
+
+      jest.spyOn(git, 'getRemoteInfo2').mockRejectedValueOnce(error);
+
+      expect(await validateGitRepositoryURL(url)).toEqual([false, error]);
+    });
+  });
+});

cli/__tests__/domains/git-repository/cloneGitRepository.spec.ts
@@ -1,0 +1,66 @@
+import git from 'isomorphic-git';
+import { wait } from '../../../src/domains/async';
+import { cloneGitRepository } from '../../../src/domains/git-repository';
+
+jest.mock('isomorphic-git');
+
+describe('cloneGitRepository | unit tests', () => {
+  let output = '';
+
+  beforeEach(() => {
+    output = '';
+
+    jest.spyOn(process.stderr, 'write').mockImplementation((message) => {
+      output += message as string;
+      return true;
+    });
+  });
+
+  it('clones git repository to the project directory', async () => {
+    const projectDir = '~/Projects/test-store';
+
+    const gitRepositoryURL = 'https://github.com/upsidelab/test-template.git';
+
+    jest.spyOn(git, 'clone').mockResolvedValueOnce();
+
+    await cloneGitRepository({
+      projectDir,
+      gitRepositoryURL
+    });
+
+    expect(git.clone).toHaveBeenCalledWith(
+      expect.objectContaining({
+        dir: projectDir,
+        url: gitRepositoryURL
+      })
+    );
+  });
+
+  it('displays progress bar that reflects cloned files', async () => {
+    jest.spyOn(git, 'clone').mockImplementationOnce(async (options) => {
+      await wait(100);
+
+      options.onProgress?.({
+        phase: 'Counting objects',
+        total: 2,
+        loaded: 1
+      });
+
+      await wait(100);
+
+      options.onProgress?.({
+        phase: 'Counting objects',
+        total: 2,
+        loaded: 2
+      });
+    });
+
+    await cloneGitRepository({
+      dir: '~/Projects/test-store',
+      gitRepositoryURL: 'https://github.com/upsidelab/test-template.git'
+    });
+
+    expect(output).toContain('Cloning https://github.com/upsidelab/test-template.git | ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ | 0%');
+    expect(output).toContain('Cloning https://github.com/upsidelab/test-template.git | ████████████████████████████████████████ | 100%');
+  });
+});