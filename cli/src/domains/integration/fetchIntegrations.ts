eb7f28483e6611530a55a37b6ab56a2d2b663a72
Rafal Kosla <rafalkosla101@gmail.com>
2022-07-26 13:19:35 +0200

    Fix eslint

--- cli/src/domains/integration/fetchIntegrations.ts
+++ cli/src/domains/integration/fetchIntegrations.ts
@@ -6,12 +6,12 @@ import {API_URL} from '../constants';
 const URL = `${API_URL}/integrations/data.json`;
 type IntegrationResponse = Omit<Integration, 'buildScriptURL'> & { buildScriptPath: string }
 const fetchIntegrations = async (): Promise<Integration[]> => {
-    const response = await fetch(URL);
-    const data = (await response.json()) as IntegrationResponse[]
-    return data.map(({buildScriptPath, ...rest}) => ({
-        ...rest,
-        buildScriptURL: API_URL.concat(buildScriptPath)
-    })) as Integration[];
+  const response = await fetch(URL);
+  const data = (await response.json()) as IntegrationResponse[];
+  return data.map(({buildScriptPath, ...rest}) => ({
+    ...rest,
+    buildScriptURL: API_URL.concat(buildScriptPath)
+  })) as Integration[];
 };
 
 export default fetchIntegrations;
--- cli/src/domains/spree/fetchSpreeTemplates.ts
+++ cli/src/domains/spree/fetchSpreeTemplates.ts
@@ -8,12 +8,12 @@ const URL = `${API_URL}/spree/data.json`;
 type SpreeResponse = Omit<Spree, 'buildScriptURL'> & { buildScriptPath: string }
 
 const fetchSpreeTemplates = async (): Promise<Spree[]> => {
-    const response = await fetch(URL);
-    const data = (await response.json()) as SpreeResponse[]
-    return data.map(({buildScriptPath, ...rest}) => ({
-        ...rest,
-        buildScriptURL: API_URL.concat(buildScriptPath)
-    })) as Spree[];
+  const response = await fetch(URL);
+  const data = (await response.json()) as SpreeResponse[];
+  return data.map(({buildScriptPath, ...rest}) => ({
+    ...rest,
+    buildScriptURL: API_URL.concat(buildScriptPath)
+  })) as Spree[];
 };
 
 export default fetchSpreeTemplates;
