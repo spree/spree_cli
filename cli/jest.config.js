34fd67d4af56f9431c0563475c6ffd1381330047
Rafal Cymerys <rafal@upsidelab.io>
2022-09-10 16:07:05 +0200

    chore: merge base files into root config files

--- cli/jest.config.js
+++ cli/jest.config.js
@@ -4,4 +4,16 @@
  * Jest's configuration object.
  * @type {import('@jest/types').Config.InitialOptions}
  */
-module.exports = require('../jest.base.config.js');
+module.exports = {
+  transform: {
+    '^.+\\.[jt]s$': 'ts-jest'
+  },
+  coverageDirectory: './coverage/',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: [
+    'src/**/*.ts'
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  watchPathIgnorePatterns: ['**/node_modules'],
+  testMatch: ['<rootDir>/**/__tests__/**/*spec.[jt]s?(x)']
+};
