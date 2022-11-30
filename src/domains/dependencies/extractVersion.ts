80ebedac84d81669a01f6039e4956b20ce910389
Filip Drozd <fpdrozd@gmail.com>
2022-11-30 15:12:51 +0100

    feat: refactor error message

--- src/domains/dependencies/extractVersion.ts
+++ src/domains/dependencies/extractVersion.ts
@@ -4,7 +4,7 @@ const extractVersion = (versionString: string): string => {
   const regexResult = versionString.trim().match(versionRegExp);
 
   if (!regexResult || !regexResult[0]) {
-    throw new Error('Invalid output');
+    throw new Error(`Invalid version string: ${versionString}`);
   }
   return regexResult[0];
 };
