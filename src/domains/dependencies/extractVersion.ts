5ee14f8999df4ed6f549a44740153a3ddaafe634
Filip Drozd <fpdrozd@gmail.com>
2022-11-30 11:51:45 +0100

    feat: refactor the version extractor

--- src/domains/dependencies/extractVersion.ts
+++ src/domains/dependencies/extractVersion.ts
@@ -1,9 +1,12 @@
-import extractVersionByRegExp from './extractVersionByRegExp';
-
 const versionRegExp = /(\d+)(\.\d+)(\.\d+)?/g;
 
-const extractVersion = (output: string): string => {
-  return extractVersionByRegExp(output, versionRegExp);
+const extractVersion = (versionString: string): string => {
+  const regexResult = versionString.trim().match(versionRegExp);
+
+  if (!regexResult || !regexResult[0]) {
+    throw new Error('Invalid output');
+  }
+  return regexResult[0];
 };
 
 export default extractVersion;
